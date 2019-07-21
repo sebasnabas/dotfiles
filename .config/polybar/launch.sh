9eb3f0fb0a18a982a1d790e0b1d2bf1b1feca951
Sebastian Eberlein <sebastianedot@gmail.com>
2019-07-21 16:44:21 +0200

    Update polybar, add bars for external monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,3 +11,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 polybar top -c ~/.config/polybar/config-top.ini &
 polybar bottom -c ~/.config/polybar/config-bottom.ini &
+
+my_laptop_external_monitor=$(xrandr --query | grep 'HDMI1')
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+    polybar top-external -c ~/.config/polybar/config-top-external.ini &
+    polybar bottom-external -c ~/.config/polybar/config-bottom-external.ini &
+fi
+
