92a106bdb017840dae2debd5735ec581b8c2e0f3
Sebastian Eberlein <sebastianedot@gmail.com>
2019-09-27 09:09:24 +0200

    Rename color to colors. Remove separate bars for external monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
-
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar top -c ~/.config/polybar/config-top.ini &
-polybar bottom -c ~/.config/polybar/config-bottom.ini &
-
-my_laptop_external_monitor=$(xrandr --query | grep 'HDMI1')
-if [[ $my_laptop_external_monitor = *connected* ]]; then
-    polybar top-external -c ~/.config/polybar/config-top-external.ini &
-    polybar bottom-external -c ~/.config/polybar/config-bottom-external.ini &
-fi
+for monitor in $(polybar --list-monitors | cut -d ":" -f1); do
+    MONITOR=$monitor polybar --reload top -c ~/.config/polybar/config-top.ini &
+    MONITOR=$monitor polybar --reload bottom -c ~/.config/polybar/config-bottom.ini &
+done
 
