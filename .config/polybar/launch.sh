fc784a7c7c6b330ebf07922af2abafe5d840dd1c
Sebastian Eberlein <sebasnabas@protonmail.com>
2020-01-22 08:47:12 +0100

    Make wifi interface dynamic

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,6 +3,8 @@
 # Terminate already running bar instances
 killall -q polybar
 
+export WIFI_IF="$(ip -br addr | grep 'UP' | cut -d ' ' -f 1)"
+
 for monitor in $(polybar --list-monitors | cut -d ":" -f1); do
     MONITOR=$monitor polybar --reload top -c ~/.config/polybar/config-top.ini &
     if [ $(cat /etc/os-release | grep '^ID' | cut -d '=' -f 2) == 'arch' ]; then
