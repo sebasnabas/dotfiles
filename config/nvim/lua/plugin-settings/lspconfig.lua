b3f8619a7991eb255d967c2b87748a4c1597c6dd
sebasnabas <sebasnabas@protonmail.com>
2023-05-21 12:54:31 +0200

    Fix omnisharp lsp setup

--- config/nvim/lua/plugin-settings/lspconfig.lua
+++ config/nvim/lua/plugin-settings/lspconfig.lua
@@ -110,9 +110,9 @@ require('rust-tools').setup({
     },
 })
 
-require('lspconfig').omnisharp.setup{
+require('lspconfig').tflint.setup({
   cmd = { "tflint", "--langserver", "--config", "~/.config/tflint/tflint.hcl" },
-}
+})
 
 --- Ui
 require("lsp_lines").setup()
@@ -126,4 +126,25 @@ vim.diagnostic.config({
   underline = true,
   update_in_insert = true,
   severity_sort = true,
+})
+
+-- HACK: https://github.com/OmniSharp/omnisharp-roslyn/issues/2483#issuecomment-1539809155
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    local function toSnakeCase(str)
+      return string.gsub(str, "%s*[- ]%s*", "_")
+    end
+
+    if client.name == 'omnisharp' then
+      local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+      for i, v in ipairs(tokenModifiers) do
+        tokenModifiers[i] = toSnakeCase(v)
+      end
+      local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+      for i, v in ipairs(tokenTypes) do
+        tokenTypes[i] = toSnakeCase(v)
+      end
+    end
+  end,
 })
