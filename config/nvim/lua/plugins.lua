211c90ab2254871606b2cff69749dda83bc64e6a
sebasnabas <sebasnabas@protonmail.com>
2024-04-11 09:37:00 +0200

    Improve telescope file picker

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -197,13 +197,33 @@ require('lazy').setup({
           require('telescope.actions').select_default(prompt_bufnr)
         end
       end
+
+      -- Make file name more visible in path
+      -- Taken from https://github.com/nvim-telescope/telescope.nvim/issues/2014#issuecomment-1873229658
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "TelescopeResults",
+        callback = function(ctx)
+          vim.api.nvim_buf_call(ctx.buf, function()
+            vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+            vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+          end)
+        end,
+      })
+      local function filenameFirst(_, path)
+        local tail = vim.fs.basename(path)
+        local parent = vim.fs.dirname(path)
+        if parent == "." then return tail end
+        return string.format("%s\t\t%s", tail, parent)
+      end
+
       require("telescope").setup({
         defaults = {
           mappings = {
             i = {
               ['<CR>'] = select_one_or_multi,
             }
-          }
+          },
+          path_display = filenameFirst
         },
         extensions = {
          fzf = {
