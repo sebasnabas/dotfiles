bb3aa92c0810316bfcd3cefbe373393a23f5e791
sebasnabas <sebasnabas@protonmail.com>
2025-04-21 09:29:32 +0200

    Remove not needed scripts

--- scripts/statusbar/system-usb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-usb_icon="󰕓"
-
-if [ -n "$(udisksctl status | grep -vE '(DEVICE|-|sda)')" ]; then
-    echo "$usb_icon "
-else
-    echo ""
-fi
--- scripts/statusbar/wireguard.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -n "$(nmcli connection show --active | grep wireguard)" ]; then
-    echo "%{T8}%{T-}"
-else
-    echo ""
-fi
-
