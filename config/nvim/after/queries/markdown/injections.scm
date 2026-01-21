3f6f9f3c26bbb615850f13b7210efdf021d8268f
sebasnabas <sebasnabas@protonmail.com>
2026-01-21 19:55:39 +0100

    Add markview header folding

--- config/nvim/after/queries/markdown/injections.scm
+++ config/nvim/after/queries/markdown/injections.scm
@@ -1,12 +1,19 @@
 ; extends
 
+; Additional language abbreviation: C#
 (fenced_code_block
   (info_string (language) @_language)
   (#any-of? @_language "cs" "csharp")
   (code_fence_content) @c_sharp)
 
+; Additional language abbreviation: Python
 (fenced_code_block
   (info_string (language) @_language)
   (#any-of? @_language "pycon")
   (code_fence_content) @python)
 
+; Markview header folding
+; NOTE: `(#trim!)` is used to prevent empty lines at the end of the section from being folded.
+((section
+    (atx_heading)) @fold
+    (#trim! @fold))
